f05b9bc2aed275573436855329e27f68826a6706
Adam Batten <adambatten@deloitte.co.uk>
2024-10-28 12:44:00 +0000

    Add loading spinner

--- src/components/widgets/Contact2.tsx
+++ src/components/widgets/Contact2.tsx
@@ -6,10 +6,16 @@ import Headline from '../common/Headline';
 import { ContactProps } from '~/shared/types';
 import WidgetWrapper from '../common/WidgetWrapper';
 import { contactSchema } from '~/shared/formValidation/contact.schema';
+import Spinner from './spinner';
+import { useState } from 'react';
 
 const Contact2 = ({ header, form, id, hasBackground = false }: ContactProps) => {
+  const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = async (data: any, doNotSaveEmail: boolean | undefined) => {
+    
+    
+    setIsLoading(true);
     await fetch('/api/sendmail', {
       method: 'POST',
       headers: {
@@ -28,14 +34,19 @@ const Contact2 = ({ header, form, id, hasBackground = false }: ContactProps) => 
         body: JSON.stringify(data),
       });
     }
+    setIsLoading(false);
   };
   return (
+    <>
+   { isLoading ? <Spinner /> :
   <WidgetWrapper id={id ? id : ''} hasBackground={hasBackground} containerClass="max-w-7xl mx-auto">
     {header && <Headline header={header} titleClass="text-3xl sm:text-5xl" />}
     <div className="flex items-stretch justify-center">
       <Form {...form} containerClass="card h-fit max-w-2xl mx-auto p-5 md:p-12" btnPosition="right" customValidation={contactSchema} customSubmission={onSubmit} />
     </div>
   </WidgetWrapper>
+}
+  </>
 )};
 
 export default Contact2;
