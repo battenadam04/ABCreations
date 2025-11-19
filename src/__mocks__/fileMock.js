67f7c941fb5dc2cd88799f88309f7cc648cec700
Adam Batten <adambatten@live.co.uk>
2025-11-19 21:06:20 +0000

    updating mock file

--- src/__mocks__/fileMock.js
+++ src/__mocks__/fileMock.js
@@ -1,2 +1,6 @@
-// __mocks__/fileMock.js
-module.exports = 'test-file-stub';
+module.exports = {
+  src: 'test-file-stub.png',
+  height: 100,
+  width: 100,
+  blurDataURL: 'data:image/png;base64,',
+};
